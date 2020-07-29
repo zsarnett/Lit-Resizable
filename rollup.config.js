084de59373e6b964029017a3c7281e373b4e1831
Zack Arnett <arnett.zackary@gmail.com>
2020-07-28 19:41:19 -0500

    add demo

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,37 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: "src/lit-resizable.ts",
-  output: {
-    file: "dist/lit-resizable.js",
-    format: "es",
+export default [
+  {
+    input: "src/lit-resizable.ts",
+    output: {
+      file: "dist/lit-resizable.js",
+      format: "es",
+    },
+    plugins: [
+      typescript(),
+      nodeResolve({
+        extensions: [".js", ".ts"],
+        preferBuiltins: false,
+        browser: true,
+      }),
+      terser(),
+    ],
   },
-  plugins: [
-    typescript(),
-    nodeResolve({
-      extensions: [".js", ".ts"],
-      preferBuiltins: false,
-      browser: true,
-    }),
-    terser(),
-  ],
-};
+  {
+    input: "src/lit-resizable-wrapper.ts",
+    output: {
+      file: "dist/lit-resizable-wrapper.js",
+      format: "es",
+    },
+    plugins: [
+      typescript(),
+      nodeResolve({
+        extensions: [".js", ".ts"],
+        preferBuiltins: false,
+        browser: true,
+      }),
+      terser(),
+    ],
+  },
+];
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,13 @@
 export interface LGLDomEvent<T> extends Event {
   detail: T;
 }
+
 export interface DraggingEvent {
   deltaX: number;
   deltaY: number;
+}
+
+export interface ResizingEvent {
+  width: number;
+  height: number;
 }
