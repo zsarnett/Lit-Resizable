38d2691ca60ccc799c680d7294ff7a5a0137d53d
Zack Arnett <arnett.zackary@gmail.com>
2020-08-02 12:07:13 -0500

    rollup config changes, v 0.0.15

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,60 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
+import filesize from "rollup-plugin-filesize";
 
 export default [
   {
     input: "src/lit-resizable.ts",
     output: {
       file: "dist/lit-resizable.js",
-      format: "es",
+      format: "esm",
       sourcemap: "inline",
     },
     plugins: [
-      typescript(),
       nodeResolve({
         extensions: [".js", ".ts"],
-        preferBuiltins: false,
-        browser: true,
       }),
-      terser(),
+      typescript(),
+      terser({
+        warnings: true,
+        ecma: 2017,
+        compress: {
+          unsafe: true,
+        },
+        output: {
+          comments: false,
+        },
+      }),
+      filesize({
+        showBrotliSize: true,
+      }),
     ],
   },
   {
     input: "src/lit-resizable-wrapper.ts",
     output: {
       file: "dist/lit-resizable-wrapper.js",
-      format: "es",
+      format: "esm",
     },
     plugins: [
-      typescript(),
       nodeResolve({
         extensions: [".js", ".ts"],
-        preferBuiltins: false,
-        browser: true,
       }),
-      terser(),
+      typescript(),
+      terser({
+        warnings: true,
+        ecma: 2017,
+        compress: {
+          unsafe: true,
+        },
+        output: {
+          comments: false,
+        },
+      }),
+      filesize({
+        showBrotliSize: true,
+      }),
     ],
   },
 ];
